28cad5c0979481ae61c1f1a06cdd5f3a9bc14352
areyou-use <arekwojdala@gmail.com>
2026-04-02 21:15:20 +0200

    Update Weather_alert.py

--- Weather_alert.py
+++ Weather_alert.py
@@ -1,1 +1,17 @@
-print("Weather test")
+from datetime import datetime
+import pandas
+import random
+import smtplib
+import os
+
+# import os and use it to get the Github repository secrets
+MY_EMAIL = os.environ.get("MY_EMAIL")
+MY_PASSWORD = os.environ.get("MY_PASSWORD")
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(MY_EMAIL, MY_PASSWORD)
+    connection.sendmail(
+        from_addr=MY_EMAIL,
+        to_addrs="arektest@onet.pl",
+        msg="Subject:Hello\n\Rain test")
